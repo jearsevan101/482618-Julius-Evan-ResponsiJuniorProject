7eda9b246d430cbdc75ce02cf76fa00be6a8d73f
jearsevan101 <juliusevanars@gmail.com>
2023-12-12 10:11:00 +0700

    first commit

---
+++ 482618-Julius Evan-ResponsiJuniorProject/482618-Julius Evan-ResponsiJuniorProject/Form1.cs
@@ -0,0 +1,10 @@
+namespace _482618_Julius_Evan_ResponsiJuniorProject
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
